5f94ae402355ef75dd1adbd523c6e25e2eeabc36
Liaa UwU <91318396+GuraBot404@users.noreply.github.com>
2021-12-15 17:39:19 +0700

    Add files via upload

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,31 +1,22 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT
 
 *🏧 BANK*
 
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
+Tidak Tersedia
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ -
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 085703155153
 3. DANA
-	ㅁ 0887 4350 47326
-	
-Semuanya atas nama RI****DI
+	ㅁ 085703155153
 
 *📲 PULSA*
 
-Tidak tersedia
+	ㅁ 085703155153
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
--- shop/donasi.js
+++ shop/donasi.js
@@ -1,27 +1,15 @@
 const donasibot = () => {
-return `DONASI BISA VIA BANK / E-MONEY
+return `DONASI VIA E-MONEY
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ -
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 085703155153
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 085703155153
 	
-Semuanya atas nama RI****DI
 `
 	}
 
